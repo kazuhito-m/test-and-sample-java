10dd179d87d21c569173832c24ca8d255af7da23
kazuhito_m <kazuhito.sumpic@gmail.com>
2013-09-01 15:18:03 +0900

    JUnit実践入門用を追加。

---
+++ src/main/java/samples/junitbook/StringUtils.java
@@ -0,0 +1,14 @@
+package samples.junitbook;
+
+public class StringUtils {
+
+    /** キャメルケースをスネークケースに変える。
+     * @param targetStr 対象文字列。
+     * @return 変換された結果文字列。 */
+    public String toSnakeCase(String targetStr) {
+        // ネットから拾ってきたコピペで事なきをえた。
+        String convertedStr = targetStr.replaceAll("([A-Z]+)([A-Z][a-z])",
+                "$1_$2").replaceAll("([a-z])([A-Z])", "$1_$2");
+        return convertedStr.toLowerCase();
+    }
+}
